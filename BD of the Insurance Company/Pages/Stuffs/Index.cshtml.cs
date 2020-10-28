69f0731b2963eed3516fafe57e63d0f3f2260895
Александра Молчанова <annakarmolina@icloud.com>
2020-10-28 12:20:59 +0300

    First

---
+++ BD of the Insurance Company/Pages/Stuffs/Index.cshtml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using BD_of_the_Insurance_Company.Data;
+using BD_of_the_Insurance_Company.Models;
+
+namespace BD_of_the_Insurance_Company.Pages.Stuffs
+{
+    public class IndexModel : PageModel
+    {
+        private readonly BD_of_the_Insurance_Company.Data.BD_of_the_Insurance_CompanyContext _context;
+
+        public IndexModel(BD_of_the_Insurance_Company.Data.BD_of_the_Insurance_CompanyContext context)
+        {
+            _context = context;
+        }
+
+        public IList<Staff> Staff { get;set; }
+        public IList<Position> Position { get; set; }
+        public async Task OnGetAsync()
+        {
+            Staff = await _context.Staff.ToListAsync();
+            Position = await _context.Position.ToListAsync();
+        }
+    }
+}
